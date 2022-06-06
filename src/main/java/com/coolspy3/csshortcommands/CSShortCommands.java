24b9d25344f971111d3e5ea395f0196dbbd5e7c0
CoolSpy3 <coolspythree@gmail.com>
2022-06-06 11:57:12 -0700

    use jitpack

--- src/main/java/com/coolspy3/csshortcommands/CSShortCommands.java
+++ src/main/java/com/coolspy3/csshortcommands/CSShortCommands.java
@@ -8,8 +8,8 @@ import com.coolspy3.csmodloader.util.Utils;
 import com.coolspy3.cspackets.packets.ClientChatSendPacket;
 
 @Mod(id = "csshortcommands", name = "CSShortCommands",
-        description = "Adds the ability to alias commands.", version = "1.0.0",
-        dependencies = {"csmodloader:[1,2)", "cspackets:[1,2)", "csutils:[1,2)"})
+        description = "Adds the ability to alias commands.", version = "1.0.1",
+        dependencies = {"csmodloader:[1.3.1,2)", "cspackets:[1.2.1,2)", "csutils:[1.1.1,2)"})
 public class CSShortCommands implements Entrypoint
 {
 
